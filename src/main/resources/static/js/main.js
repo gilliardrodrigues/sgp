eb11fb7d4c9dc69b69601fdb3f3cab3624e31ee1
Fernando Moreira <fernandoepm.pro@gmail.com>
2022-10-27 00:16:54 -0300

    Subindo frontend da parte adminsitartiva

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,10 @@
+
+function expandFilters() {
+  document.getElementsById('filters-expanded').style.display = 'block';
+  alert("how are you");  
+  // var div = document.getElementsById('card-filtro');
+
+  // div.addEventListener('click', function (event) {
+  //   document.getElementsById('filters-expanded').style.display = 'block';
+  // });
+}
