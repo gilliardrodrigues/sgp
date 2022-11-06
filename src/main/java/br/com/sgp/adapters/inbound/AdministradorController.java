ba2b6f8ce493bfed3c152255bbf5d107c809e1e0
samuelWas <samuelwillianalmeida@gmail.com>
2022-11-06 15:10:45 -0300

    feat: adicionado tela de login

--- src/main/java/br/com/sgp/adapters/inbound/AdministradorController.java
+++ src/main/java/br/com/sgp/adapters/inbound/AdministradorController.java
@@ -10,6 +10,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/administradores")
 @AllArgsConstructor
 public class AdministradorController {
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,10 +1,41 @@
-
 function expandFilters() {
-  document.getElementsById('filters-expanded').style.display = 'block';
-  alert("how are you");  
-  // var div = document.getElementsById('card-filtro');
+	document.getElementsById("filters-expanded").style.display = "block";
+	alert("how are you");
+}
 
-  // div.addEventListener('click', function (event) {
-  //   document.getElementsById('filters-expanded').style.display = 'block';
-  // });
+function addEventListeners() {
+	const loginForm = document.querySelector("#loginForm");
+	loginForm.addEventListener("submit", function (e) {
+		submitForm(e, this);
+	});
+}
+
+async function submitForm(e, form) {
+	e.preventDefault();
+
+	const headers = {
+		"Content-Type": "application/json",
+	};
+
+	const jsonFormData = buildJsonFormData(form);
+
+	await fetch("http://localhost:8080/administradores", {
+		method: "POST",
+		headers,
+		body: JSON.stringify(jsonFormData),
+	}).then(response => {
+		if (!response.ok) {
+			response.json().then(body => alert(body.titulo));
+		} else {
+			location.href = "homeAdmin/index.html";
+		}
+	});
+}
+
+function buildJsonFormData(form) {
+	const jsonFormData = {};
+	for (const pair of new FormData(form)) {
+		jsonFormData[pair[0]] = pair[1];
+	}
+	return jsonFormData;
 }
