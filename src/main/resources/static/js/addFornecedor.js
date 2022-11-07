74508be4846a72b95904e15653b636620ed737aa
samuelWas <samuelwillianalmeida@gmail.com>
2022-11-06 22:25:18 -0300

    feat: implementado telas de adicionar fornecedor

---
+++ src/main/resources/static/js/addFornecedor.js
@@ -0,0 +1,75 @@
+window.onload = async () => {
+	const form = document.querySelector("form");
+	form.addEventListener("submit", function (e) {
+		submitForm(e, this);
+	});
+};
+
+async function submitForm(e, form) {
+	e.preventDefault();
+
+	const headers = {
+		"Content-Type": "application/json",
+	};
+
+	const jsonFormData = buildJsonFormData(form);
+
+	jsonFormData.produtosOferecidos = [];
+	if (jsonFormData.caneca) {
+		delete jsonFormData.caneca;
+		jsonFormData.produtosOferecidos.push("CANECA");
+	}
+	if (jsonFormData.tirante) {
+		delete jsonFormData.tirante;
+		jsonFormData.produtosOferecidos.push("TIRANTE");
+	}
+	if (jsonFormData.camisa) {
+		delete jsonFormData.camisa;
+		jsonFormData.produtosOferecidos.push("CAMISA");
+	}
+
+	let comentario;
+	if (jsonFormData.observacao) {
+		comentario = jsonFormData.observacao;
+		delete jsonFormData.observacao;
+	}
+
+
+	await criarFornecedor(headers, jsonFormData, comentario);
+
+	location.href = "../fornecedores/index.html";
+}
+
+async function criarFornecedor(headers, jsonFormData, comentario) {
+	await fetch("http://localhost:8080/fornecedores", {
+		method: "POST",
+		headers,
+		body: JSON.stringify(jsonFormData),
+	}).then(response => {
+		if (!response.ok) {
+			response.json().then(body => {
+				alert(body.titulo);
+			});
+		} else {
+			response.json().then(body => {
+				criarComentario(headers, body.id, comentario);
+			});
+		}
+	});
+}
+
+async function criarComentario(headers, fornecedorId, comentario) {
+	await fetch(`http://localhost:8080/fornecedores/${fornecedorId}/observacoes`, {
+		method: "POST",
+		headers,
+		body: JSON.stringify({ comentario: comentario }),
+	});
+}
+
+function buildJsonFormData(form) {
+	const jsonFormData = {};
+	for (const pair of new FormData(form)) {
+		jsonFormData[pair[0]] = pair[1];
+	}
+	return jsonFormData;
+}
