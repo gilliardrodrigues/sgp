c9d500413e7b0a74e52d1e32bb73eb07ffd8642e
samuelWas <samuelwillianalmeida@gmail.com>
2022-11-08 01:23:19 -0300

    fix: removido evento não utilizado em fornecedor

--- src/main/resources/static/js/fornecedor.js
+++ src/main/resources/static/js/fornecedor.js
@@ -5,11 +5,6 @@ window.onload = async () => {
 		const fornecedorHTML = montarHTMLFornecedor(fornecedor);
 		content.append(fornecedorHTML);
 	}, []);
-
-	const deleteForm = document.querySelector(".delete-button");
-	deleteForm.addEventListener("submit", function (e) {
-		submitForm(e, this);
-	});
 };
 
 async function getFornecedores() {
@@ -36,7 +31,7 @@ function montarHTMLFornecedor(fornecedor) {
 		tabela.appendChild(col);
 	});
 
-	tabela.innerHTML += `<div class="column ver-obs w12"> <a href="../verObsFornecedor/index.html?id=${fornecedor.id}"> <button type="submit" style="border: 0; background: transparent"> <p>Ver<br>observações</p></button> </a> </div><div class="column edit-fornecedor w8"> <a href="../editarFornecedor/index.html?id=${fornecedor.id}"> <button type="submit" class="edit-button" style="border: 0; background: transparent"> <img src="../../static/img/edit-button.svg" width="20px" alt="submit"/> </button> </a> </div><div class="column delete-button w5 last-column"> <a href=""> <button onclick="removerFornecedor(${fornecedor.id})"style="border: 0; background: transparent"> <img src="../../static/img/trash-icon.png" width="20px" alt="submit"/> </button> </a> </div>`
+	tabela.innerHTML += `<div class="column ver-obs w12"> <a href="../verObsFornecedor/index.html?id=${fornecedor.id}"> <button type="submit" style="border: 0; background: transparent"> <p>Ver<br>observações</p></button> </a> </div><div class="column edit-fornecedor w8"> <a href="../editarFornecedor/index.html?id=${fornecedor.id}"> <button type="submit" class="edit-button" style="border: 0; background: transparent"> <img src="../../static/img/edit-button.svg" width="20px" alt="submit"/> </button> </a> </div><div class="column delete-button w5 last-column"> <button onclick="removerFornecedor(${fornecedor.id})"style="border: 0; background: transparent"> <img src="../../static/img/trash-icon.png" width="20px" alt="submit"/> </button></div>`
 	
 	return tabela;
 }
@@ -46,26 +41,4 @@ async function removerFornecedor(id) {
 		method: "DELETE",
 	});
 	location.reload();
-}
-
-async function submitForm(e, form) {
-	e.preventDefault();
-
-	const headers = {
-		"Content-Type": "application/json",
-	};
-
-	const jsonFormData = buildJsonFormData(form);
-
-	await fetch(`http://localhost:8080/fornecedores/${id}`, {
-		method: "DELETE",
-		headers,
-		body: JSON.stringify(jsonFormData),
-	}).then(response => {
-		if (!response.ok) {
-			response.json().then(body => alert(body.titulo));
-		} else {
-			location.href = "homeAdmin/index.html";
-		}
-	});
 }
