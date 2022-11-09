07e621cc7fd6e753ffd6f7ff50de7249caceb2f2
Fernando Moreira <fernandoepm.pro@gmail.com>
2022-11-09 00:13:24 -0300

    Editar temporada

---
+++ src/main/resources/static/js/editarTemporada.js
@@ -0,0 +1,63 @@
+window.onload = async () => {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	const id = urlParams.get("id");
+
+	const form = document.querySelector("form");
+	form.addEventListener("submit", function (e) {
+		submitForm(e, this, id);
+	});
+
+	//const situacao = ["AguardandoPagamento", "Confirmado", "ParcialmenteEntregue", "Entregue"];
+
+	const temporada = await fetch(`http://localhost:8080/temporadas/${id}`).then(response => response.json());
+
+  console.log(temporada);
+	document.querySelector(".descricao").value = temporada.descricao;
+	//document.querySelector(".inicio").value = temporada.inicio;
+	//document.querySelector(".termino").value = temporada.termino;
+	// document.querySelector(".tempo-entrega").value = temporada.tempoEntregaEmDias;
+	/*
+	temporada.produtosOferecidos.forEach(produto => {
+		if (produto === "Caneca") {
+			document.querySelector(".caneca").checked = true;
+		} else if (produto === "Tirante") {
+			document.querySelector(".tirante").checked = true;
+		} else if (produto === "Camisa") {
+			document.querySelector(".camisa").checked = true;
+		}
+	});*/
+};
+
+async function submitForm(e, form, id) {
+	e.preventDefault();
+
+	const headers = {
+		"Content-Type": "application/json",
+	};
+
+	const jsonFormData = buildJsonFormData(form);
+
+	editarTemporada(headers, jsonFormData, id);
+}
+async function editarTemporada(headers, jsonFormData, id) {
+	console.log({
+		url: `http://localhost:8080/temporadas/${id}`,
+		method: "PUT",
+		headers,
+		body: JSON.stringify(jsonFormData),
+	});
+	await fetch(`http://localhost:8080/temporadas/${id}`, {
+		method: "PUT",
+		headers,
+		body: JSON.stringify(jsonFormData),
+	}).then(() => (location.href = "../temporadas/index.html"));
+}
+
+function buildJsonFormData(form) {
+	const jsonFormData = {};
+	for (const pair of new FormData(form)) {
+		jsonFormData[pair[0]] = pair[1];
+	}
+	return jsonFormData;
+}
