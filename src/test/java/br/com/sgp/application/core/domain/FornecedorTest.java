fa1f70a331710c27e922cdef39cf0b9c91f2fc2f
Gilliard Gabriel Rodrigues <gilliard2019@ufmg.br>
2022-12-02 01:17:24 -0300

    refactor: padronizando os nomes dos testes do domain.

--- src/test/java/br/com/sgp/application/core/domain/FornecedorTest.java
+++ src/test/java/br/com/sgp/application/core/domain/FornecedorTest.java
@@ -2,16 +2,12 @@ package br.com.sgp.application.core.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import br.com.sgp.application.core.domain.Fornecedor;
-
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FornecedorTest {
 
     @Test
-    void cadastroDeObservacao() {
+    void testCadastroDeObservacao() {
         Fornecedor fornecedor = new Fornecedor(1L, "razaoSocial", "123456789", "email", 20);
 
         assertEquals(0, fornecedor.getObservacoes().size());
