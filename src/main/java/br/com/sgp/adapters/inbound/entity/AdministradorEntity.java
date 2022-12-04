ab351384b42af1a8a143e34425a8b59ba7af9154
samuelWas <samuelwillianalmeida@gmail.com>
2022-12-04 16:22:03 -0300

    fix: adicionado construtor em AdministradorEntity

--- src/main/java/br/com/sgp/adapters/inbound/entity/AdministradorEntity.java
+++ src/main/java/br/com/sgp/adapters/inbound/entity/AdministradorEntity.java
@@ -2,6 +2,7 @@ package br.com.sgp.adapters.inbound.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
@@ -14,6 +15,7 @@ import javax.persistence.*;
 @Setter
 @Table(name = "ADMINISTRADOR")
 @AllArgsConstructor
+@NoArgsConstructor
 public class AdministradorEntity {
 
     @Id
